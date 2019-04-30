045931f0b744d2f3cebe5d6b593bf2e7d0500417
DESKTOP-D351Q1F\Bartek <bac2@vp.pl>
2019-04-30 17:09:34 +0200

    renamed plotting file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import data_service as ds
-import plot_data as plot
+import plot_service as plot
 test_images, test_labels =  ds.load_test_data()
 train_images, train_labels = ds.load_train_data(limit_for_single_letter=10)
 plot.plot_image_array(train_images, train_labels, columns_number=10)
