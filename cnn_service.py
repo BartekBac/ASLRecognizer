ffa488d59804fbbc3a8a646471a0fec9ac2484ee
DESKTOP-D351Q1F\Bartek <bac2@vp.pl>
2019-05-30 19:31:34 +0200

    working at creating session, handling problem with oftmax_cross_entropy_with_logits_sg function

--- cnn_service.py
+++ cnn_service.py
@@ -18,29 +18,32 @@ def max_pool(inputs, ksize=[1,2,2,1], strides=[1,1,1,1], padding='SAME'):
             )
 
 def cnn_model(input_layer, dropout=0.75):
-    filter_c1 = tf.random_normal([5,5,3,32]) 
+
+    input_layer = tf.reshape(input_layer, shape=[-1, 64,64,3])
+
+    filter_c1 = tf.Variable(tf.random_normal([5,5,3,32]))
     conv1 = conv2d(input_layer, filter=filter_c1, strides=[1,1,1,1])
     pool1 = max_pool(conv1)
     
-    filter_c2 = tf.random_normal([5,5,32,64])
+    filter_c2 = tf.Variable(tf.random_normal([5,5,32,64]))
     conv2 = conv2d(pool1, filter=filter_c2, strides=[1,1,1,1])
     pool2 = max_pool(conv2)
 
-    weights_f = tf.random_normal([16*16*64, 1024]) 
+    weights_f = tf.Variable(tf.random_normal([16*16*64, 1024]))
     fully_c = tf.reshape(pool2, [-1,16*16*64])
     fully_c = tf.nn.relu(
                 tf.add(
                     tf.matmul(fully_c, weights_f), 
-                    tf.random_normal([1024])
+                    tf.Variable(tf.random_normal([1024]))
                 ))  
 
     fully_c = tf.nn.dropout(fully_c, rate=dropout)
 
     classes_quantity = 29
-    weights_l = tf.random_normal([1024, classes_quantity ])
+    weights_l = tf.Variable(tf.random_normal([1024, classes_quantity ]))
     logits = tf.add(
                 tf.matmul(fully_c, weights_l),
-                tf.random_normal([classes_quantity]) 
+                tf.Variable(tf.random_normal([classes_quantity]))
                 )
 
     predictions = tf.nn.softmax(logits)
--- main.py
+++ main.py
@@ -2,10 +2,97 @@ import data_service as ds
 import plot_service as plot
 import tensorflow as tf
 import cnn_service as cnn
+from time import time
+import numpy as np
 
-test_images, test_labels =  ds.load_test_data()
-train_images, train_labels = ds.load_train_data(limit_for_single_letter=10)
-#plot.plot_image_array(train_images, train_labels, columns_number=10)
+count_of_single_letter = 30
 
-for image in train_images:
-    prediction = cnn.cnn_model(image)
+train_images, train_labels = ds.load_train_data_fixed(limit_for_single_letter=count_of_single_letter)
+
+# Parameters
+learning_rate = 0.001
+batch_size = 64
+training_iters = 500 
+display_step = 50
+# Network Parameters
+n_classes = 29
+n_input = 64*64*3
+dropout = 0.75 # Dropout, probability to keep units
+
+# tf Graph input
+x = tf.placeholder(tf.float32, [None, n_input])
+y = tf.placeholder(tf.float32, [None, n_classes])
+keep_prob = tf.placeholder(tf.float32) #dropout (keep probability)
+
+##for image in train_images:
+##    prediction = cnn.cnn_model(image)
+##    print(prediction)
+
+##random_images, random_labels = ds.get_random_batch(train_images, train_labels, 25)
+##print('Drawn labels:')
+##print(random_labels)
+
+# Construct model
+#pred = conv_net(x, weights, biases, keep_prob)
+prediction = cnn.cnn_model(x, dropout)
+# prepare loss and optimizer variables
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+# evaluate model
+correct_prediction = tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+######
+# Initializing the variables
+#init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
+
+losses = list()
+accuracies = list()
+saver = tf.train.Saver()
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+    
+    step = 0
+    epoch = 0
+    start_epoch = time()
+ 
+    # Keep training until reach max iterations
+    while step <= training_iters:
+
+        random_images, random_labels = ds.get_random_batch(train_images, train_labels, batch_size)
+        
+        # Fit training using batch data
+        start_op = time()
+        sess.run(optimizer, feed_dict={x: random_images, y: random_labels, keep_prob: dropout})
+        end_op = time()
+        print("#{} opt step {} {} takes {}".format(step,start_op,end_op, end_op-start_op))            
+        
+        if step % display_step == 0:
+            
+            print("acc start {}".format(time()))
+            # Calculate batch accuracy
+            acc = sess.run(accuracy, feed_dict={x: random_images, y: random_labels, keep_prob: 1.})
+            accuracies.append(acc)
+            
+            print("loss start {}".format(time()))
+            # Calculate batch loss
+            batch_loss = sess.run(loss, feed_dict={x: random_images, y: random_labels, keep_prob: 1.})
+            losses.append(batch_loss)
+            
+            print("Iter " + str(step) + " started={}".format(time()) + ", Minibatch Loss= " + "{}".format(batch_loss) + ", Training Accuracy= " + "{}".format(acc))
+            
+            epoch += 1
+        
+        step += 1     
+        
+    end_epoch = time()
+    print("Optimization Finished, end={} duration={}".format(end_epoch,end_epoch-start_epoch))
+    
+    
+    test_size = min(400, train_images.shape[0])
+    test_X = train_images[0:test_size,:]
+    test_Y = train_labels[0:test_size,:]
+    # Calculate accuracy 
+    print("Testing Accuracy:", sess.run(accuracy, feed_dict={x: test_X, y: test_Y, keep_prob: 1.}))
